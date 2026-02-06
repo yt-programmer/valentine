2ba65865f2eb0e9ceeb286a80425c9fa29f017de
Youssef tarek <ytelmalwany@gmail.com>
2026-02-06 07:35:38 +0200

    فثسف

--- js/main.js
+++ js/main.js
@@ -8,10 +8,10 @@ let count = 0;
 
 const noBtnFun = () => {
   count++;
-  if (count >= 5) {
+  if (count > 5) {
     noBtn.style.display = "none";
     alert.innerHTML =
-      "<div class='no-valentine' ><p>enter on the screen to exit from here </p><h1>stop messing with me</h1> <img src='images/gun.avif' alt='heart' /></div>";
+      "<div class='no-valentine' ><h1>stop messing with me</h1> <img src='images/gun.avif' alt='heart' /></div>";
   }
 
   let randomX;
@@ -27,7 +27,7 @@ const noBtnFun = () => {
   noBtn.style.transform = `translate(${randomX}px, ${randomY}px)`;
 };
 noBtn.addEventListener("mouseover", noBtnFun);
-noBtn.addEventListener("touchstart", noBtnFun);
+// noBtn.addEventListener("touchstart", noBtnFun);
 
 // noBtn.addEventListener("click", () => {
 //   noBtn.style.display = "none";
