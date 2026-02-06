0b542337cfad25ee9843d89bea57f5bf03933c47
Youssef tarek <ytelmalwany@gmail.com>
2026-02-06 07:57:32 +0200

    finish

--- js/main.js
+++ js/main.js
@@ -6,13 +6,22 @@ const noValentine = document.querySelector(".no-valentine");
 
 let count = 0;
 
+const exitWindow = (e) => {
+  if (e.target.closest(".no-valentine")) {
+    alert.innerHTML = "";
+  }
+};
+
 const noBtnFun = () => {
   count++;
   if (count > 5) {
     noBtn.style.display = "none";
-    alert.innerHTML =
-      "<div class='no-valentine' ><h1>stop messing with me</h1> <img src='images/gun.avif' alt='heart' /></div>";
+    alert.innerHTML = `<div class='no-valentine' > <button class='btn exit'>Exit </button> <h1>stop messing with me</h1> <img src='images/gun.avif' alt='heart' /></div>`;
 
+    const exitBtn = alert.querySelector(".exit");
+    exitBtn.addEventListener("click", () => {
+      alert.innerHTML = "";
+    });
     return;
   }
 
@@ -31,20 +40,18 @@ const noBtnFun = () => {
 noBtn.addEventListener("mouseover", noBtnFun);
 noBtn.addEventListener("touchstart", noBtnFun);
 
-// noBtn.addEventListener("click", () => {
-//   noBtn.style.display = "none";
-//   alert.innerHTML =
-//     "<div class='no-valentine' ><p>enter on the screen to exit from here </p> <h1>stop messing with me</h1> <img src='images/gun.avif' alt='heart' /></div>";
-// });
+noBtn.addEventListener("click", () => {
+  noBtn.style.display = "none";
+  alert.innerHTML = `<div class='no-valentine' > <button class='btn exit'>Exit </button> <h1>stop messing with me</h1> <img src='images/gun.avif' alt='heart' /></div>`;
+
+  const exitBtn = alert.querySelector(".exit");
+  exitBtn.addEventListener("click", () => {
+    alert.innerHTML = "";
+  });
+});
 
 yesBtn.addEventListener("click", () => {
   alert.innerHTML =
     "<div class='valentine'><h1>you are my valentine, i love you</h1><img src='images/Love.png' alt='heart' /> </div>";
   container.style.display = "none";
 });
-
-// alert.addEventListener("click", (e) => {
-//   if (e.target.closest(".no-valentine")) {
-//     alert.innerHTML = "";
-//   }
-// });
