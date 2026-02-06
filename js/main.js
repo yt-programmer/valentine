3b8a53fa50656f4b30da981332ae57894d7d76a1
Youssef tarek <ytelmalwany@gmail.com>
2026-02-06 06:19:30 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const noBtn = document.querySelector(".no");
+const alert = document.querySelector(".alert");
+const yesBtn = document.querySelector(".yes");
+const container = document.querySelector(".container");
+
+let count = 0;
+noBtn.addEventListener("mouseover", () => {
+  count++;
+  if (count > 5) {
+    noBtn.style.display = "none";
+    alert.innerHTML =
+      "<div class='no-valentine' ><h1>stop messing with me</h1> <img src='images/gun.avif' alt='heart' /></div>";
+  }
+  const randomX = Math.floor(Math.random() * 500);
+  const randomY = Math.floor(Math.random() * 500);
+  noBtn.style.transform = `translate(${randomX}px, ${randomY}px)`;
+});
+
+noBtn.addEventListener("click", () => {
+  noBtn.style.display = "none";
+  alert.innerHTML =
+    "<div class='no-valentine' ><h1>stop messing with me</h1> <img src='images/gun.avif' alt='heart' /></div>";
+});
+
+yesBtn.addEventListener("click", () => {
+  alert.innerHTML =
+    "<div class='valentine'><h1>you are my valentine, i love you</h1><img src='images/love.png' alt='heart' /> </div>";
+  container.style.display = "none";
+});
